89653c996e2b34b7285571bf92ec83a9ad359143
Jamshid <jsaminjonov5705@gmail.com>
2023-12-05 10:39:49 +0500

    git delete

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Houzing from "./components";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Houzing />
+  </React.StrictMode>
+);
